379d99c0dbe7dc75c1aaf5bf3cc7811cbf8a1a29
softwarelordcode <softwarelordcode@users.noreply.github.com>
2025-05-26 12:29:40 +0200

    first commit

---
+++ jpg_to_png_converter.py
@@ -0,0 +1,14 @@
+'''
+This script converts JPG images to PNG format.
+Usage: python jpg_to_png_converter.py <input_directory> <output_directory>
+'''
+
+import sys
+import os
+from PIL import Image
+
+# grab first and second argument
+# check is new/ exists, if not create
+# loop through Pokedex,
+# convert images to png
+# save to the new
