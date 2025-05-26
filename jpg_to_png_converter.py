8699a86d3a186899731b6bb2c3e98f04635129f4
softwarelordcode <softwarelordcode@users.noreply.github.com>
2025-05-26 13:57:34 +0200

    check if source and output exist

--- jpg_to_png_converter.py
+++ jpg_to_png_converter.py
@@ -7,8 +7,19 @@ import sys
 import os
 from PIL import Image
 
-# grab first and second argument
-# check is new/ exists, if not create
+try:
+    source = sys.argv[1]
+    output = sys.argv[2]
+except IndexError:
+    print("Usage: python jpg_to_png_converter.py <input_directory> <output_directory>")
+    sys.exit(1)
+
+if not os.path.exists(source):
+    print("Input directory does not exist.")
+    sys.exit(1)
+if not os.path.exists(output):
+    os.makedirs(output)
+
 # loop through Pokedex,
 # convert images to png
 # save to the new
