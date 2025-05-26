2fd8bb150a6240127b891019deae8977d74acc81
softwarelordcode <softwarelordcode@users.noreply.github.com>
2025-05-26 14:34:25 +0200

    loop and convert

--- jpg_to_png_converter.py
+++ jpg_to_png_converter.py
@@ -17,16 +17,20 @@ except IndexError:
 if not os.path.exists(source):
     print("Input directory does not exist.")
     sys.exit(1)
-if not os.path.exists(output):
-    os.makedirs(output)
+
+os.makedirs(output, exist_ok=True)
 
 files = os.listdir(source)
-
 jpg_files = filter(lambda file: os.path.splitext(file)[1].lower() == '.jpg', files)
 
 for file in jpg_files:
+    f, e = os.path.splitext(file)
+    outfile = f + ".png"
     print(f'Converting {file} to PNG format...')
+    try:
+        with Image.open(os.path.join(source, file)) as im:
+            im.save(os.path.join(output, outfile), 'PNG')
+    except OSError as err:
+        print(f"Cannot convert {err}")
 
-# loop through Pokedex,
-# convert images to png
-# save to the new
+print("Conversion completed.")
